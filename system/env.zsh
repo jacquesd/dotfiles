c63e37ce7da6285d264cde1ef41163777302253d
Jacques Dafflon <jacques.dafflon@gmail.com>
2016-11-23 15:56:25 +0100

    Gurobi license and atom config crap

--- system/env.zsh
+++ system/env.zsh
@@ -3,5 +3,6 @@ export JAVA_HOME=`/usr/libexec/java_home`
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export GRB_LICENSE_FILE='/Users/kodikodytis/Documents/licenses/gurobi.lic'	
 
 eval `/usr/libexec/path_helper -s`
